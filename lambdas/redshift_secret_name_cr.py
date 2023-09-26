0411f75e8715bfdd82783ca7f1ae74f3e295d650
Tarun Menon <tarunmenon95@gmail.com>
2023-09-26 15:09:27 +1000

    Fix cr function name

--- lambdas/redshift_secret_name_cr.py
+++ lambdas/redshift_secret_name_cr.py
@@ -4,6 +4,6 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-def lambda_handler(event, context):
+def handler(event, context):
     logger.info(event)
     return boto3.client('secretsmanager').describe_secret(SecretId=event['ResourceProperties']['SecretArn'])['Name']
